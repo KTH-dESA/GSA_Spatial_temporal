5a53a7bc1f1136fbbd91aebc7f7bc3e3e53333c2
Nandi Moksnes <nandi.moksnes@gmail.com>
2022-08-26 15:40:27 +0200

    modified sensitivity for results

--- src/Sensitivity_analysis.py
+++ src/Sensitivity_analysis.py
@@ -32,28 +32,29 @@ modified_values = param_values.copy()
 # We then `floor` the values for the categorical inputs
 # e.g., everything < 1 will be mapped to 0, between 1.0 and 2.0 as 1, etc
 modified_values[:, 0:3] = np.floor(modified_values[:, 0:3])
-np.savetxt("sample_morris.csv", modified_values, delimiter=",")
+np.savetxt("sensitivity/sample_morris.csv", modified_values, delimiter=",")
 
 # To use optimized trajectories (brute force method),
 # give an integer value for optimal_trajectories
 
 # Run the "model" -- this will happen offline for external models
 # Modified values that are floored
-#Y = Sobol_G.evaluate(modified_values)
+results = np.loadtxt("sensitivity/Morris_results.csv", dtype=int)
+Y = results[0]
 
 # Perform the sensitivity analysis using the model output
 # Specify which column of the output file to analyze (zero-indexed)
-#Si = morris.analyze(problem, param_values, Y, conf_level=0.95,
-#                    print_to_console=True,
-#                    num_levels=4, num_resamples=100)
+Si = morris.analyze(problem, param_values, Y, conf_level=0.95,
+                    print_to_console=True,
+                    num_levels=4, num_resamples=100)
 # Returns a dictionary with keys 'mu', 'mu_star', 'sigma', and 'mu_star_conf'
 # e.g. Si['mu_star'] contains the mu* value for each parameter, in the
 # same order as the parameter file
 
-#fig, (ax1, ax2) = plt.subplots(1, 2)
-#horizontal_bar_plot(ax1, Si, {}, sortby='mu_star', unit=r"tCO$_2$/year")
-#covariance_plot(ax2, Si, {}, unit=r"tCO$_2$/year")
+fig, (ax1, ax2) = plt.subplots(1, 2)
+horizontal_bar_plot(ax1, Si, {}, sortby='mu_star', unit=r"kUSD")
+covariance_plot(ax2, Si, {}, unit=r"kUSD")
 
-#fig2 = plt.figure()
-#sample_histograms(fig2, param_values, problem, {'color': 'y'})
-#plt.show()
+fig2 = plt.figure()
+sample_histograms(fig2, param_values, problem, {'color': 'y'})
+plt.show()
