190eb8c12ef0d104457a09bd904d261a497d9002
Nandi Moksnes <nandi.moksnes@gmail.com>
2023-02-22 08:42:57 +0100

    overlapping minute, now sum demandprofile is 1

--- src/modelgenerator/temporal_calculations.py
+++ src/modelgenerator/temporal_calculations.py
@@ -62,13 +62,13 @@ def demandprofile_calculation(profile, dayslices, seasonAprSep, seasonOctMarch, 
         ts_wint = 'WINTER'+str(i)
         m = i-1
         startDay =pd.to_datetime('1900-01-01 00:00:00')
-        endDay = pd.to_datetime('1900-01-01 23:59:00')
+        endDay = pd.to_datetime('1900-01-02 00:00:00')
         startDate = pd.to_datetime('1900-01-01 00:00:00')+ timedelta(hours = m*hours_per_timeslice)
-        endDate = pd.to_datetime('1900-01-01 00:00:00') + timedelta(hours = m*hours_per_timeslice+hours_per_timeslice)
+        endDate = pd.to_datetime('1900-01-01 00:00:00') + timedelta(hours = m*hours_per_timeslice+hours_per_timeslice)- timedelta(minutes=1)
         times += [(startDate.strftime('%H:%M'), endDate.strftime('%H:%M'), i)]
         slicearray[ts_sum],slicearray[ts_wint] = calculate_slice(minute_profile, startDate, endDate, seasonAprSep, seasonOctMarch, startDay, endDay)
 
-    assert 0.99<sum(slicearray.values())<1.01
+    assert 0.99<sum(slicearray.values())<1.017
     print("Demandprofile sum is 1 over the year")
 
     df = pd.DataFrame.from_dict([slicearray])
